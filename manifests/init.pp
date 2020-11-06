d6a4a7c4d87c85af4a4a9745a6e9feaa940098fd
Maarten Beeckmans <maartenb@inuits.eu>
2020-11-06 21:16:55 +0100

    add basic

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,19 @@
 #
-class profile_template (
+class profile_docker (
+  Hash    $registries                = {},
+  Boolean $remove_stopped_containers = true,
 ) {
+  class { 'docker': }
+
+  create_resources('docker::registry', $registries)
+
+  if $remove_stopped_containers {
+    cron { 'remove stopped containers, unused volumes, unused networks and dangling images every hour':
+      command     => 'docker system prune -f > /dev/null 2>&1',
+      user        => 'root',
+      hour        => '*/1',
+      minute      => 42,
+      environment => 'PATH=/bin:/usr/bin',
+    }
+  }
 }
